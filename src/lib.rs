8a283c035a6d1a6d4f75178bb7cc04a596dd8819
ShubhamSalucro <sgame@salucro.com>
2022-02-08 17:03:19 +0530

    Added Integrations & Test Cases

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev:: Server;
+use std::net::TcpListener;
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("Hello");
+    format!("Hellow {}!", &name)
+}
+async fn health() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+// #[tokio::main]
+pub fn run(listener: TcpListener) -> Result<Server,std::io:: Error> {
+   let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))   
+            .route("/greet/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod:: run;
+use std::{net::TcpListener};
+
+#[tokio:: main]
+async fn main() -> std::io:: Result<()> {
+    let listener = TcpListener:: bind("127.0.0.1:0").unwrap();
+    // println!("{:?}",listener.local_addr().unwrap().ip());
+// Bubble up the io::Error if we failed to bind the address
+// Otherwise call .await on our Server
+run(listener)?.await
 }
